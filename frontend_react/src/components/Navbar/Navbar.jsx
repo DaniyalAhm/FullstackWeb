39140c56f099e287b6b4d1db7db998f6153d293d
Daniyal <daniyala@bu.edu>
2024-01-04 04:55:48 -0600

    Half way done with nav bar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import './Navbar.scss'
+import React from 'react';
+import './Navbar.scss';
+
+import { images } from '../../constant-imports';
 
 function Navbar() {
+  const menuItems = ['Home', 'About', 'Footer', 'Header', 'Personal Projects', 'Skills'];
+
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <nav className='app__navbar'>
+      <div className = "app__navbar-logo">
+        <img src={images.logo}  alt="logo" />
+      </div>
+
+    
+    <ul className='app__navbar-links'> {(() => {
+      const listItems = [];
+
+      for (let i = 0; i < menuItems.length; i++) {
+        const item = menuItems[i];
+        listItems.push(
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+      );
+    }
+
+    return listItems;
+  })()}
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ frontend_react/src/constant-imports/images.js
@@ -0,0 +1,6 @@
+// constant-imports.js
+import logo from '../Assets/D.png';
+
+export default {
+    logo
+};
