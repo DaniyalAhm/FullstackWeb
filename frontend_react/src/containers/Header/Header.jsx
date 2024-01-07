2cfcab276a3c63db8ce3cac512d89fc35d0e4806
Daniyal <daniyala@bu.edu>
2024-01-07 09:53:29 -0600

    Have to restructure the css files for my own implementation

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './Header.scss'
+import {motion} from 'framer-motion'
+import {images} from '../../constant-imports'
+
+
+
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className='Header'>
+      <motion.div 
+       
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 2, ease: "easeInOut" }}
+        className= "Header__info"
+
+      >
+
+        <div className='Header-badge'>
+
+        <div className='badge-cmp app__flex'>
+
+        <p className='Intro'>  Hello, I am Daniyal</p>
+        <h1 className='Header__name'>Daniyal Ahmed</h1>
+        
+
+        </div>
+
+        </div>
+
+      </motion.div>
     </div>
   )
 }
